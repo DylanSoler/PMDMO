83cd898b6a685a7874647aa41419e1c1173b5ece
Unknown <dylanadriansoler@gmail.com>
2018-12-13 22:22:55 +0100

    Comenzando

---
+++ Sudokaso/app/src/main/java/com/example/dasoler/sudokaso/Main2Activity.java
@@ -0,0 +1,14 @@
+package com.example.dasoler.sudokaso;
+
+import android.app.ListActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main2Activity extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
