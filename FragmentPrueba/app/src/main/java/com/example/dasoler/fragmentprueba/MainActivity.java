34d437b3f0d363e51ae6baed932943fbaee97a07
Unknown <dylanadriansoler@gmail.com>
2018-12-14 20:37:38 +0100

    Comenzando con Fragment

--- FragmentPrueba/app/src/main/java/com/example/dasoler/fragmentprueba/MainActivity.java
+++ FragmentPrueba/app/src/main/java/com/example/dasoler/fragmentprueba/MainActivity.java
@@ -1,27 +1,13 @@
-package com.example.dasoler.sudokaso;
+package com.example.dasoler.fragmentprueba;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
-    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        btn = (Button)findViewById(R.id.button);
-
-        btn.setOnClickListener(this);
-
-    }
-
-    @Override
-    public void onClick(View view) {
-
     }
 }
--- Sudokaso/app/src/main/java/com/example/dasoler/sudokaso/Main2Activity.java
+++ Sudokaso/app/src/main/java/com/example/dasoler/sudokaso/Main2Activity.java
@@ -1,14 +1,29 @@
 package com.example.dasoler.sudokaso;
 
 import android.app.ListActivity;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Main2Activity extends ListActivity {
+public class Main2Activity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        if(findViewById(R.id.frlayoutsudoku)==null){
+
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            ListFragment frag1 = new ListFragment();
+
+            frag1.setArguments(getIntent().getExtras());
+
+            getSupportFragmentManager().beginTransaction().add(R.id.frlayout, frag1).commit();
+
+        }
     }
 }
